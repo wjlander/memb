ec3e08dfe812b4f4dd23679179af5fce6cfda326
wjlander <will@w-j-lander.uk>
2025-09-30 11:33:55 +0100

    Updated ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -88,7 +88,8 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
-    pmx: true
+    pmx: true,
+    env: {
       NODE_ENV: 'production',
       PORT: 3000,
       // Add any other environment variables needed

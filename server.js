1cf2187967cf9a708d1b252fd3ef74a7af3cab2d
wjlander <will@w-j-lander.uk>
2025-09-30 11:51:28 +0100

    Fix Express Route Pattern and PM2 Config

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/favicon.ico', (req, res) => {
 });
 
 // Handle client-side routing - serve index.html for all non-API routes
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
